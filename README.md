c0bcae2f8f95fc76cc96b084805b76bb65519eb5
TangYu <TangYu-T_Y@outlook.com>
2021-12-28 16:05:09 +0800

    reconsitution,2021/12/28 by @T_Y

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Word2Vec
 train to get Vector ，word Embedding
+
+已用jupyter重构
+直接运行即可在gpu上训练
+
+
+It has been reconstructed with jupyter and can be trained on GPU by running directly。
+
+训练模型结构如下
+![img.png](Picture/img.png)
--- save.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-from Modul import EmbeddingModel
-
-if __name__ == '__main__':
-    net = EmbeddingModel(1000, 100)
-    net.cuda()
-    for i in range(10):
-        torch.save(net, './model/embedding_epoch{}_.pt'.format(i))
